8bb5564751ec9fc36b9414970c427f922e92d9fd
komp23 <kukushonok.kukuhka@gmail.com>
2023-12-12 03:08:17 +0300

    не работает 3 этап но сделан

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
-
-const name = readlineSync.question("fgdfg");
-
-console.log(name);
+import name from "../src/cli";

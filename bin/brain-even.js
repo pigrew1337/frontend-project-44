2116c647aa7977f809b1cf91f97d5d0d9c6b43fc
komp23 <kukushonok.kukuhka@gmail.com>
2023-12-20 01:25:56 +0300

    надо сделать разные числа

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,52 +1,50 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
-// Question 79
 
-userName();
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 // cli.js скопировал +console.log
+function getRandomNumber() {
+  return Math.floor(Math.random() * (100));
+}
+const randomNumber = getRandomNumber();
 // рандомное число
 for (let i = 0; i < 3; i += 1) {
-    function getRandomNumber() {
-      return Math.floor(Math.random() * (100));
-    }
-    const randomNumber = getRandomNumber();
-    // рандомное число
-    // выводим рандомсное чисол
-    console.log(`Question: ${randomNumber}`);
-    // выводим рандомсное чисол
-    // вводи да или нет
-    const answer = readlineSync.question('Your answer: ');
-    // вводи да или нет
-    if (randomNumber % 2 === 0 && answer === 'yes') {
-      console.log('Correct!');
-    }
-    if (randomNumber % 2 !== 0 && answer === 'no') {
-      console.log('Correct!');
-    }
-    // вводи да или нет
-    // ответил неправиильно
-    if (randomNumber % 2 === 0 && answer === 'no') {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
-      console.log("Let's try again, Bill!");
-      break;
-    }
-    if (randomNumber % 2 !== 0 && answer === 'yes') {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'`);
-      console.log("Let's try again, Bill!");
-      break;
-    }
-    if (answer !== 'no' && answer !== 'yes' && randomNumber % 2 === 0) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
-      console.log("Let's try again, Bill!");
-      break;
-    }
-    if (answer !== 'no' && answer !== 'yes' && randomNumber % 2 !== 0) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'`);
-      console.log("Let's try again, Bill!");
-      break;
-    }
-  // ответил неправиильно
+  // рандомное число
+  // выводим рандомсное чисол
+  console.log(`Question: ${randomNumber}`);
+  // выводим рандомсное чисол
+  // вводи да или нет
+  const answer = readlineSync.question('Your answer: ');
+  // вводи да или нет
+  if (randomNumber % 2 === 0 && answer === 'yes') {
+    console.log('Correct!');
   }
-console.log(`Congratulations, ${userName}!`);
+  if (randomNumber % 2 !== 0 && answer === 'no') {
+    console.log('Correct!');
+  }
+  // вводи да или нет
+  // ответил неправиильно
+  if (randomNumber % 2 === 0 && answer === 'no') {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  if (randomNumber % 2 !== 0 && answer === 'yes') {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  if (answer !== 'no' && answer !== 'yes' && randomNumber % 2 === 0) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  if (answer !== 'no' && answer !== 'yes' && randomNumber % 2 !== 0) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+  // ответил неправиильно
+}
